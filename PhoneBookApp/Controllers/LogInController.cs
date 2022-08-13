82c08800f4cc2204bd12acb8a98af147c488fda0
Sowvik Sarker <ssowvik@gmail.com>
2022-08-13 10:47:03 +0600

    Login Implemented

--- PhoneBookApp/Controllers/LogInController.cs
+++ PhoneBookApp/Controllers/LogInController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBookApp.Data;
+using PhoneBookApp.Models;
 
 namespace PhoneBookApp.Controllers
 {
     public class LogInController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public LogInController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(User obj)
+        {
+            var UserInfo = _db.Users.FirstOrDefault(u=>u.UserName == obj.UserName && u.Password == obj.Password);
+            if(UserInfo == null)
+            {
+                return NotFound();
+            }
+            return View("~/Views/Home/Index.cshtml", obj);
         }
     }
 }
